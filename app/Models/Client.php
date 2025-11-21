2743afe374fe303f45be1c32d18c40de048547d8
Firen <the.firen.ex@gmail.com>
2025-11-21 15:15:03 +0000

    LAB6 - Utworzenie fabryki php artisan make:factory ClientFactory --model=Client

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
-    //
+    use SoftDeletes;
+    use \Illuminate\Database\Eloquent\Factories\HasFactory;
+
+    protected $fillable = [
+        'Imie',
+        'Nazwisko',
+        'Nazwa_Firmy',
+        'PESEL_NIP',
+        'Adres_Ulica',
+        'Adres_Nr_Domu',
+        'Adres_Nr_Lokalu',
+        'Adres_Miejscowosc',
+        'Adres_Kod_Pocztowy',
+        'Nr_Telefonu',
+        'Email_Adres',
+        'Zdolnosc_Kredytowa',
+        'Uwagi',
+    ];
 }
