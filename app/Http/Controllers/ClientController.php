911c5516e6824f5501e1a324da9bb328dbd29ff8
Firen <the.firen.ex@gmail.com>
2025-11-21 17:11:55 +0000

    LAB6 - Dodanie wyświetlania tabeli klientów w sidebar(kontroler, szablon). Dodanie tłumaczenia tabeli Client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
     public function index(Request $request)
     {
-        dd('ClientController');
+
+        $clients = Client::paginate(20);
+
+        return view('clients.index', compact('clients'));
+
     }
 }
